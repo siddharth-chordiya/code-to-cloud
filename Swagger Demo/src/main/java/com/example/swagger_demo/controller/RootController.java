37d3cfabeca0c6bffd64a7e743ff3adbf8dd8656
Admin <Admin@DESKTOP-1K1DO8H>
2026-04-02 21:31:59 +0530

    added docker file and root controller

---
+++ Swagger Demo/src/main/java/com/example/swagger_demo/controller/RootController.java
@@ -0,0 +1,16 @@
+package com.example.swagger_demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("health")
+    public String healthCheck() {
+        return "Service is UP";
+    }
+
+}
